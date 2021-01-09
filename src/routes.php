a1482f8f11c0c41576fd092d7b6da5bbb5a3cf39
Rafealka007 <dolnickova.sandra@gmail.com>
2021-01-09 15:36:08 +0100

    bugs removed and added again

--- src/routes.php
+++ src/routes.php
@@ -25,63 +25,63 @@ $app->post('/test', function (Request $request, Response $response, $args) {
 
 /* Zoznam vsech osob v DB */
 $app->get('/persons', function (Request $request, Response $response, $args) {
-	$stmt = $this->db->query('SELECT * FROM person ORDER BY first_name'); # toto vrati len DB objekt, nie vysledok!
-	$tplVars['persons_list'] = $stmt->fetchall(); # [ ['id_person' => 1, 'first_name' => 'Alice' ... ], ['id_person' => 2, 'first_name' => 'Bob' ... ] . ]
-	return $this->view->render($response, 'persons.latte', $tplVars);
+    $stmt = $this->db->query('SELECT * FROM person ORDER BY first_name'); # toto vrati len DB objekt, nie vysledok!
+    $tplVars['persons_list'] = $stmt->fetchall(); # [ ['id_person' => 1, 'first_name' => 'Alice' ... ], ['id_person' => 2, 'first_name' => 'Bob' ... ] . ]
+    return $this->view->render($response, 'persons.latte', $tplVars);
 });
 
 
 $app->get('/search', function (Request $request, Response $response, $args) {
-	$queryParams = $request->getQueryParams(); # [kluc => hodnota]
-	if(! empty($queryParams) ) {
-		$stmt = $this->db->prepare("SELECT * FROM person WHERE lower(first_name) = lower(:fname) OR lower(last_name) = lower(:lname)");
-		$stmt->bindParam(':fname', $queryParams['q']);
-		$stmt->bindParam(':lname', $queryParams['q']);
-		$stmt->execute();
-		$tplVars['persons_list'] = $stmt->fetchall();
-		return $this->view->render($response, 'persons.latte', $tplVars);
-	}
+    $queryParams = $request->getQueryParams(); # [kluc => hodnota]
+    if(! empty($queryParams) ) {
+        $stmt = $this->db->prepare("SELECT * FROM person WHERE lower(first_name) = lower(:fname) OR lower(last_name) = lower(:lname)");
+        $stmt->bindParam(':fname', $queryParams['q']);
+        $stmt->bindParam(':lname', $queryParams['q']);
+        $stmt->execute();
+        $tplVars['persons_list'] = $stmt->fetchall();
+        return $this->view->render($response, 'persons.latte', $tplVars);
+    }
 })->setName('search');
 
 
 /* nacitanie formularu */
 $app->get('/person', function (Request $request, Response $response, $args) {
-	return $this->view->render($response, 'newPerson.latte');
+    return $this->view->render($response, 'newPerson.latte');
 })->setName('newPerson');
 
 /* spracovanie formu po odoslani */
 $app->post('/person', function (Request $request, Response $response, $args) {
-	$formData = $request->getParsedBody();
-	$tplVars = [];
-	if ( empty($formData['first_name']) || empty($formData['last_name']) || empty($formData['nickname']) ) {
-		$tplVars['message'] = 'Please fill required fields';
-	} else {
-		try {
+    $formData = $request->getParsedBody();
+    $tplVars = [];
+    if ( empty($formData['first_name']) || empty($formData['last_name']) || empty($formData['nickname']) ) {
+        $tplVars['message'] = 'Please fill required fields';
+    } else {
+        try {
             $this->db->beginTransaction();
             if( !empty($formData["street_name"]) || !empty($formData["street_number"])
                 || !empty($formData["city"]) || !empty($formData["zip"])){
                 $id_location = newLocation($this, $formData);
-                }
+            }
 
-			$stmt = $this->db->prepare("INSERT INTO person (nickname, first_name, last_name, id_location, birth_day, height, gender) VALUES (:nickname, :first_name, :last_name, :id_location, :birth_day, :height, :gender)");
-				$stmt->bindValue(':nickname', $formData['nickname']);
-				$stmt->bindValue(':first_name', $formData['first_name']);
-				$stmt->bindValue(':last_name', $formData['last_name']);
-				$stmt->bindValue(':id_location', $id_location ? $id_location : null);
-				$stmt->bindValue(':gender', empty($formData['gender']) ? null : $formData['gender'] ) ;
-				$stmt->bindValue(':birth_day', empty($formData['birth_day']) ? null : $formData['birth_day']);
-				$stmt->bindValue(':height', empty($formData['height']) ? null : $formData['height']);
-				$stmt->execute();
-				$tplVars['message'] = 'Person succefully added';
-                $this->db->commit();
-		} catch (PDOexception $e) {
-			$tplVars['message'] = 'Error occured, sorry jako';
-			$this->logger->error($e->getMessage());
+            $stmt = $this->db->prepare("INSERT INTO person (nickname, first_name, last_name, id_location, birth_day, height, gender) VALUES (:nickname, :first_name, :last_name, :id_location, :birth_day, :height, :gender)");
+            $stmt->bindValue(':nickname', $formData['nickname']);
+            $stmt->bindValue(':first_name', $formData['first_name']);
+            $stmt->bindValue(':last_name', $formData['last_name']);
+            $stmt->bindValue(':id_location', $id_location ? $id_location : null);
+            $stmt->bindValue(':gender', empty($formData['gender']) ? null : $formData['gender'] ) ;
+            $stmt->bindValue(':birth_day', empty($formData['birth_day']) ? null : $formData['birth_day']);
+            $stmt->bindValue(':height', empty($formData['height']) ? null : $formData['height']);
+            $stmt->execute();
+            $tplVars['message'] = 'Person succefully added';
+            $this->db->commit();
+        } catch (PDOexception $e) {
+            $tplVars['message'] = 'Error occured, sorry jako';
+            $this->logger->error($e->getMessage());
             $this->db->rollback();
-		}
-	}
+        }
+    }
 
-	$this->view->render($response, 'newPerson.latte', $tplVars);
+    $this->view->render($response, 'newPerson.latte', $tplVars);
     $basePath = $request->getUri()->getBasePath();
     return $response->withRedirect($basePath."/persons");
 });
@@ -290,21 +290,21 @@ $app->get('/meeting/update', function (Request $request, Response $response, $ar
 $app->post("/meeting/update", function (Request $request, Response $response, $args){
     $id_meeting = $request->getQueryParam('id_meeting');
     $formData = $request->getParsedBody();
-        try {
-            $stmt = $this->db->prepare("UPDATE meeting SET start = :start, 
+    try {
+        $stmt = $this->db->prepare("UPDATE meeting SET start = :start, 
 														  decription = :description,
 														  duration = :duration, 
 														  id_location = :id_location 
 													  WHERE id_meeting = :id_meeting");
-            $stmt->bindValue(":start", $formData["start"]);
-            $stmt->bindValue(":description", $formData["description"]);
-            $stmt->bindValue(":duration", $formData["duration"]);
-            $stmt->bindValue(":id_location", $formData["id_location"]);
-            $stmt->bindValue(":meeting", $id_meeting);
-            $stmt->execute();
-        } catch (PDOexception $e) {
-            $tplVars['message'] = 'Error occured in updatemeeting';
-        }
+        $stmt->bindValue(":start", $formData["start"]);
+        $stmt->bindValue(":description", $formData["description"]);
+        $stmt->bindValue(":duration", $formData["duration"]);
+        $stmt->bindValue(":id_location", $formData["id_location"]);
+        $stmt->bindValue(":meeting", $id_meeting);
+        $stmt->execute();
+    } catch (PDOexception $e) {
+        $tplVars['message'] = 'Error occured in updatemeeting';
+    }
     $tplVars["formData"] = $formData;
     $this->view->render($response, 'updateMeeting.latte', $tplVars);
     $basePath = $request->getUri()->getBasePath();
@@ -359,15 +359,15 @@ $app->post('/meeting', function (Request $request, Response $response, $args) {
 $app->post('/meeting/delete', function (Request $request, Response $response, $args) {
     $id_meeting = $request->getQueryParam('id_meeting');
     echo ("Mám deprese");
-        try {
-            $stmt = $this->db->prepare('DELETE FROM meeting WHERE id_meeting = :id_meeting');
-            $stmt->bindValue(':id_meeting', $id_meeting);
+    try {
+        $stmt = $this->db->prepare('DELETE FROM meeting WHERE id_meeting = :id_meeting');
+        $stmt->bindValue(':id_meeting', $id_meeting);
 
-            $stmt->execute();
-        } catch (PDOexception $e) {
-            $this->logger->error($e->getMessage());
-            exit('error occured');
-        }
+        $stmt->execute();
+    } catch (PDOexception $e) {
+        $this->logger->error($e->getMessage());
+        exit('error occured');
+    }
 
     $basePath = $request->getUri()->getBasePath();
     return $response->withRedirect($basePath."/meetings");
